3b7dc49a51bb4cf84317103113789f9d2e519cb6
VP <vpp41998@gmail.com>
2023-05-01 02:21:10 +0530

    Added routes

--- Models/dataModel.js
+++ Models/dataModel.js
@@ -1,7 +1,23 @@
 const db = require("mongoose");
 
 const dataSchema = new db.Schema({
-
+    govtidType: { type: String },
+    govtid: { type: String },
+    pincode: { type: Number },
+    country: { type: String },
+    state: { type: String },
+    city: { type: String },
+    address: { type: String },
+    email: { type: String },
+    relation: { type: String },
+    emergencyContact: { type: Number },
+    guardianName: { type: String },
+    nationality: { type: String },
+    occupation: { type: String },
+    mobile: { type: Number },
+    gender: { type: String },
+    age: { type: Number },
+    name: { type: String }
 });
 
 module.exports = db.model("data", dataSchema);
--- Routes/dataRoute.js
+++ Routes/dataRoute.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const DATA = require("../Models/dataModel");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    const data = await DATA.find().sort({ name: 1 });
     res.json({
-        message: "Hello"
+        data
+    })
+});
+
+app.post("/", async (req, res) => {
+    await DATA.create(req.body);
+    res.json({
+        message: "Data added"
+    })
+})
+
+app.delete("/:id", async (req, res) => {
+    const id = req.params.id
+    await DATA.deleteOne({ _id: id });
+    res.json({
+        message: "Data deleted"
     })
 })
 
